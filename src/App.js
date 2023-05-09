c90ba98710358718246f91fe79e81b8633373b89
huetruong06062002 <truongvshieu01@gmail.com>
2023-05-09 20:37:53 +0700

    video 19

--- src/App.js
+++ src/App.js
@@ -3,26 +3,42 @@ import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import MyComponent from "./components/MyComponent";
+import React from "react";
 
-const App = () => {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
+//Viết theo kiểu class components
+class App extends React.Component {
+  state = {
+    name: "Eric",
+    address: "Hoi Dan IT",
+    age: 21,
+  };
 
-  return (
-    <div>
-      Hello Word!
-      <MyComponent></MyComponent>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>Hello word Hoi dan it</p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        Hello Word!
+        <MyComponent></MyComponent>
+      </div>
+    );
+  }
+}
+
+//Viết theo kiểu function components
+// const App = () => {
+//   const count = useSelector((state) => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Hello word Hoi dan it</p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// };
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,12 +5,27 @@ import React from "react";
 class MyComponent extends React.Component {
   // MyComponent kế thừa lại lớp Components của react
 
+  state = {
+    name: "Eric",
+    address: "Hoi Dan IT",
+    age: 26,
+  };
+
+  handleClick(event) {
+    console.log(">> Click me my button");
+    console.log("My name is ", this.state.name);
+  }
+
+  handleOnMoverOver(event) {
+    console.log(event.pageX);
+  }
   //JSX: cho phép viết JS bên trong html
   render() {
     return (
       <div>
-        My first component
-        {Math.random()}
+        My name is {this.state.name} and I'm from {this.state.address}
+        <button onMouseOver={this.handleOnMoverOver}>Hover me</button>
+        <button onClick={this.handleClick}>Click me</button>
       </div>
     );
   }
