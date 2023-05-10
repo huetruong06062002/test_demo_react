476f91518658af87917a5f05a749371c2dcd2ee5
huetruong06062002 <truongvshieu01@gmail.com>
2023-05-10 20:17:28 +0700

    video 28

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./DisplayInfor.scss";
+import logo from "../logo.svg";
 
 class DisplayInfor extends React.Component {
   state = {
@@ -19,6 +20,7 @@ class DisplayInfor extends React.Component {
     //props => viết tắt properties
     return (
       <div className="display-infor-container">
+        <img src={logo} />
         <div>
           <h1
             onClick={() => {
