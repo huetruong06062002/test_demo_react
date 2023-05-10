63df6255b2e03a0bada567475f195e4e099f8fae
huetruong06062002 <truongvshieu01@gmail.com>
2023-05-10 17:27:31 +0700

    video 24

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,14 +2,21 @@ import React from "react";
 
 class DisplayInfor extends React.Component {
   render() {
-    console.log(this.props);
     //destructuring
-    const { age, name } = this.props;
+    const { listUsers } = this.props;
+    console.log(listUsers);
     //props => viết tắt properties
     return (
       <div>
-        <div>My name is {name}</div>
-        <div>My age is {age}</div>
+        {listUsers.map((user) => {
+          return (
+            <div key={user.id}>
+              <div>My name is {user.name} </div>
+              <div>My age is {user.age}</div>
+              <hr />
+            </div>
+          );
+        })}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,15 +6,23 @@ import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
   // MyComponent kế thừa lại lớp Components của react
-
+  state = {
+    listUsers: [
+      { id: 1, name: "Hoi Dan IT", age: "30" },
+      { id: 2, name: "Eric", age: "26" },
+      { id: 3, name: "Henrry Phạm Dev", age: "27" },
+    ],
+  };
   //JSX: cho phép viết JS bên trong html
   render() {
     const myInfor = ["ab", "c", "d"];
     return (
       <div>
         <UserInfor />
-        <DisplayInfor name="Hoi Dan IT" age="30 " />
-        <DisplayInfor name={"Eric"} age={58} myInfor={myInfor} />
+        <DisplayInfor
+          listUsers={this.state.listUsers}
+          users={this.state.listUsers}
+        />
       </div>
     );
   }
