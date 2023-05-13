96c63b2d2d3e9d17d98e6fcef2c4e4d506130860
huetruong06062002 <truongvshieu01@gmail.com>
2023-05-13 21:52:51 +0700

    video46

--- src/App.js
+++ src/App.js
@@ -1,38 +1,7 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
-import React from "react";
 
-//Viết theo kiểu class components
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        Hello Word!
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
-}
-
-//Viết theo kiểu function components
-// const App = () => {
-//   const count = useSelector((state) => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>Hello word Hoi dan it</p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// };
+const App = () => {
+  return <div className="app-container">Hello world</div>;
+};
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,57 +4,7 @@ import React, { useState } from "react";
 import AddUserInfor from "./AddUserInfor";
 import DisplayInfor from "./DisplayInfor";
 
-// class MyComponent extends React.Component {
-//   // MyComponent kế thừa lại lớp Components của react
-//   constructor(props) {
-//     super(props);
-//     //bable compliler
-//     this.state = {
-//       listUsers: [
-//         { id: 1, name: "Hoi Dan IT", age: "16" },
-//         { id: 2, name: "Eric", age: "26" },
-//         { id: 3, name: "Henrry Phạm Dev", age: "69" },
-//       ],
-//     };
-//   }
 
-//   handleAddNewUser = (userObj) => {
-//     // let listUsersNew = this.state.listUsers;
-//     // listUsersNew.unshift(userObj);
-//     // this.setState({
-//     //   listUsers: listUsersNew,
-//     // });
-
-//     this.setState({
-//       listUsers: [userObj, ...this.state.listUsers],
-//     });
-//   };
-
-//   handleDeleteUser = (userId) => {
-//     let listUsersClone = this.state.listUsers;
-//     listUsersClone = listUsersClone.filter((item) => item.id !== userId);
-//     this.setState({
-//       listUsers: listUsersClone,
-//     });
-//   };
-
-//   //JSX: cho phép viết JS bên trong html
-//   render() {
-//     const myInfor = ["ab", "c", "d"];
-//     return (
-//       <>
-//         <div className="a">
-//           <AddUserInfor handleAddNewUser={this.handleAddNewUser} />
-//           <DisplayInfor
-//             listUsers={this.state.listUsers}
-//             handleDeleteUser={this.handleDeleteUser}
-//           />
-//         </div>
-//         <div className="b"></div>
-//       </>
-//     );
-//   }
-// }
 
 const MyComponent = (props) => {
   let [listUsers, setListUsers] = useState([
